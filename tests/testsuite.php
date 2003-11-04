99ff5d42d7cccf0a242d6c6c6eeb7d277987e6f7
Michael Wallner <mike@php.net>
2003-11-04 16:06:25 +0000

    added test case for File_Passwd and File_Passwd_Smb
    git-svn-id: http://svn.php.net/repository/pear/packages/File_Passwd/trunk@143788 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/testsuite.php
+++ tests/testsuite.php
@@ -1,17 +1,21 @@
 <?php
 class TC {
     function runTest($name){
-        include_once('test_' . strToLower($name) . '.php');
-        echo "# Starting unit test for File_Passwd_$name:\n";
-    	$tc = &new PhpUnit_TestSuite('File_Passwd_'.$name.'Test');
+        $test = strToLower($name);
+        include_once("test_$test.php");
+        echo "# Starting unit test for $name:\n";
+    	$tc = &new PhpUnit_TestSuite('File_Passwd'.($name!='File_Passwd' ? '_'.$name : '').'Test');
         $rs = PHPUnit::run($tc);
         echo $rs->toString() . "\n";
+        flush();
     }
 }
-
+echo "\n";
+TC::runTest('File_Passwd');
 TC::runTest('Common');
+TC::runTest('Smb');
+TC::runTest('Cvs');
 TC::runTest('Unix');
 TC::runTest('Authbasic');
 TC::runTest('Authdigest');
-TC::runTest('Cvs');
 ?>
