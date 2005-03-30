10042ae12640296d1de6cca335640369ec59cd23
Michael Wallner <mike@php.net>
2005-03-30 18:33:33 +0000

    * new docblock header
    git-svn-id: http://svn.php.net/repository/pear/packages/File_Passwd/trunk@183190 c90b9560-bf6c-de11-be94-00142212c4b1

--- Passwd/Authdigest.php
+++ Passwd/Authdigest.php
@@ -1,24 +1,25 @@
 <?php
-// +----------------------------------------------------------------------+
-// | PEAR :: File :: Passwd :: Authdigest                                 |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is available at http://www.php.net/license/3_0.txt              |
-// | If you did not receive a copy of the PHP license and are unable      |
-// | to obtain it through the world-wide-web, please send a note to       |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2003-2004 Michael Wallner <mike@iworks.at>             |
-// +----------------------------------------------------------------------+
-//
-// $Id$
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
-* Manipulate AuthDigestFiles as used for HTTP Digest Authentication.
-*
-* @author   Michael Wallner <mike@php.net>
-* @package  File_Passwd
-*/
+ * File::Passwd::Authdigest
+ * 
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   FileFormats
+ * @package    File_Passwd
+ * @author     Michael Wallner <mike@php.net>
+ * @copyright  2003-2005 Michael Wallner
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/File_Passwd
+ */
 
 /**
 * Requires File::Passwd::Common
